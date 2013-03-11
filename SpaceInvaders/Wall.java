441991b5409b0b423afdaf886f2cb56853432908
Foster McLane <fkmclane@gmail.com>
2013-03-10 21:27:01 -0400

    Fixes

--- SpaceInvaders/Wall.java
+++ SpaceInvaders/Wall.java
@@ -1,17 +1,16 @@
 //package spaceinvaders;
 
-import info.gridworld.actor.Rock;
 import java.awt.Color;
 
 public class Wall extends Rock {
     private int strength;
+
     public Wall(int s) {
         super(Color.BLACK);
         strength = s;
     }
     
-    public void reduceStrength()
-    {
+    public void reduceStrength() {
         strength--;
         if(strength <= 0)
         removeSelfFromGrid();
